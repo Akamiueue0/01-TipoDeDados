a84648da1461ddf00fe9ed00403391388fed6a1c
Eduardo <aikamibbs@gmail.com>
2026-09-15 11:22:17 -0300

    alterei o semestre

--- 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
+++ 1_TipoDados/TiposDeDados/TiposDeDados/TiposDeDados.cpp
@@ -6,10 +6,10 @@ using namespace std;
 
 int main()
 {
-	cout << "Estrutura de Dados - Fatec (2026/1)" << endl;
+	cout << "Estrutura de Dados - Fatec (2026/2)" << endl;
 
 	
-	cout << "Tipos Numericos\n";
+	cout << "Tipos Numericos\n";/
 	cout << "===============\n";
 	
 	int i = 10;
